03ba00d0a55f4a5078f4bcf94b35ebef66fac048
Anthony Giuliano <124478794+ACGiuliano@users.noreply.github.com>
2023-11-16 17:07:30 -0500

    Version 1.0
    Program fully functional with base features.

--- CalCounter/CalorieCounter/Program.cs
+++ CalCounter/CalorieCounter/Program.cs
@@ -9,16 +9,15 @@
 decimal userWeight;
 string? weightString;
 string? userMeal;
-string currentMeal;
+string currentMeal = "";
 
 decimal maxCalories;
 int mealInput;
 
 bool weightValid = false;
+bool doAgain = false;
 
-double breakfastCalories;
-double lunchCalories;
-double dinnerCalories;
+
 double totalCalories = 0;
 
 Console.WriteLine("Please insert your weight in pounds: ");
@@ -53,79 +52,100 @@ This will maintain your current weight.");
 
 Console.WriteLine(); // add line break for ease of reading
 
-Console.WriteLine(@"Please enter a digit 1-4 to select which of the below meals you wish to add calories to:
-                    1. Breakfast
-                    2. Lunch
-                    3. Dinner
-                    4. Snacks");
-
-userMeal = Console.ReadLine();
-
-bool isValid = int.TryParse(userMeal, out mealInput);
-
-while (isValid = false || mealInput > 4 || mealInput < 1)
+/*
+* Loop to allow user to input calories per meal,
+* allows user to decide if they want to input additional
+* meals after each itereation.
+*/
+do
 {
-    Console.WriteLine("INVALID INPUT. Please select one of the above options using numbers 1-4");
-    isValid = int.TryParse(Console.ReadLine(), out mealInput);
-}
 
-switch (mealInput)
-{
-    case 1:
-        currentMeal = "Breakfast";
-        break;
-    case 2:
-        currentMeal = "Lunch";
-        break;
-    case 3:
-        currentMeal = "Dinner";
-        break;
-    case 4:
-        currentMeal = "Snack";
-        break;
-}
+    double breakfastCalories = 0;
+    double lunchCalories = 0;
+    double dinnerCalories = 0;
+    double snackCalories = 0;
 
-breakfastCalories = CalAdd("Breakfast");
+    Console.WriteLine(@"Please enter a digit 1-4 to select which of the below meals you wish to add calories to:
+                        1. Breakfast
+                        2. Lunch
+                        3. Dinner
+                        4. Snacks");
 
+    userMeal = Console.ReadLine();
 
+    bool isValid = int.TryParse(userMeal, out mealInput);
+
+    while (isValid = false || mealInput > 4 || mealInput < 1)
+    {
+        Console.WriteLine("INVALID INPUT. Please select one of the above options using numbers 1-4");
+        isValid = int.TryParse(Console.ReadLine(), out mealInput);
+    }
+
+    /* 
+    * May combine switch and if statements,
+    * feels like if statement a bit redundant
+    */
+    switch (mealInput)
+    {
+        case 1:
+            currentMeal = "Breakfast";
+            break;
+        case 2:
+            currentMeal = "Lunch";
+            break;
+        case 3:
+            currentMeal = "Dinner";
+            break;
+        case 4:
+            currentMeal = "Snack";
+            break;
+    }
+
+    if (currentMeal.Equals("Breakfast"))
+    {
+        breakfastCalories = CalAdd(currentMeal);
+        totalCalories += breakfastCalories;
+    }
+    else if (currentMeal.Equals("Lunch"))
+    {
+        lunchCalories = CalAdd(currentMeal);
+        totalCalories += lunchCalories;
+    }
+    else if (currentMeal.Equals("Diiner"))
+    {
+        dinnerCalories = CalAdd(currentMeal);
+        totalCalories += dinnerCalories;
+    }
+    else if (currentMeal.Equals("Snacks"))
+    {
+        snackCalories = CalAdd(currentMeal);
+        totalCalories += snackCalories;
+    }
+
+    Console.WriteLine();
+
+    Console.WriteLine($"Current total calories eaten for today: {totalCalories} calories.");
+    Console.WriteLine();
+
+    Console.WriteLine("Do you want to add more calories? (Y/N)");
+    string? addMore = Console.ReadLine();
+
+    while (addMore == null || !addMore.ToUpper().Equals("Y") || !addMore.ToUpper().Equals("N"))
+    {
+        Console.WriteLine("INVALID. Please enter Y or N.");
+        addMore = Console.ReadLine();
+    }
+
+    if (addMore.ToUpper().Equals("Y"))
+        doAgain = true;
+
+} while (doAgain == true);
 
 
 /*
-* outdated loop now that method implemented 
-do
-{
-    string? breakfastInput = Console.ReadLine();
-
-    while (breakfastInput == null || breakfastInput.Length == 0)
-    {
-        Console.WriteLine("INVALID. Please enter a value for total breakfast calories.");
-        breakfastInput = Console.ReadLine();
-    }
-
-    if (Double.TryParse(breakfastInput, out breakfastCalories))
-    {
-        // validating if calorie value is positive number
-        if (breakfastCalories < 0)
-        {
-            Console.WriteLine("INVALID. Calories must be a positive number. Please enter breakfast calories eaten.");
-            break;
-        }
-        else if (breakfastCalories >= 0)
-        {
-            Console.WriteLine($"Breakfast calories {breakfastCalories}.");
-            Console.WriteLine();
-            totalCalories += breakfastCalories;
-            breakfastValid = true;
-        }
-    }
-    else 
-    {
-        Console.WriteLine($"{breakfastInput} is not a valid response. Please enter a valid number.");
-    }
-
-} while (breakfastValid == false);
-
-Console.WriteLine($"So far you have consumed {totalCalories} calories.");
+* Method to add calories, takes user input
+* and converts it to number after validation
+* then returns that number to assign to variable.
 */
 
 static double CalAdd(string meal)
@@ -133,6 +153,8 @@ static double CalAdd(string meal)
     bool validInput = false;
     double caloriesAdded = 0;
     string? calorieString;
+
+    Console.WriteLine($"Please enter calories consumed for {meal.ToLower()}");
 
     do
     {
